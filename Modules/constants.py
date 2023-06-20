d003c3834d16d87c8b30ab0b29885225167c70c3
Mario Garcia <mandres_garcia@hotmail.com>
2023-06-20 10:30:52 -0500

    Cambio a DB aroundprod

--- Modules/constants.py
+++ Modules/constants.py
@@ -4,11 +4,11 @@ json_path="./json/vars.json"
 log_Path="./Logs" # Test
 src_Path="/mnt/ingbox/LogsCDN/"
 
-data_base_connect="host=10.10.130.152 dbname=aroundtest user=vodaplications password=V0D-20234pl1c4t10ns" #use (main) (functions:extract_xml_data, Duration_Transform)
+data_base_connect="host=10.10.130.152 dbname=aroundprod user=vodaplications password=V0D-20234pl1c4t10ns" #use (main) (functions:extract_xml_data, Duration_Transform)
 Bucket_logs='logs-consolidate' #Use (functions:Dowload_Logs)
 Bucket_logs_old='logs-consolidate-old' #Use (functions:Dowload_Logs)
 aws_profile='pythonapps' #Use (functions:Dowload_Logs, extract_xml_data)
-Mail_To='mgarcia@vcmedios.com.co' #Use (functions:SendMail)
+Mail_To='ingenieriavcmc@vcmedios.com.co' #Use (functions:SendMail)
 
 dict_summary={}
 dict_manifest={}
